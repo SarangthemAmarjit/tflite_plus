f43eafe9553416a244984d2304ff88f8b83f8010
Sarangthem Amarjit Meetei <sarangthemamarjit123@gmail.com>
2025-05-05 08:14:18 +0530

    Updated First

--- test/tflite_test.dart
+++ test/tflite_test.dart
@@ -2,7 +2,8 @@ import 'dart:typed_data';
 
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:tflite/tflite.dart';
+import 'package:tflite_plus/tflite.dart';
+
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
